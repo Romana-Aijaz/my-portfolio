2ebfe8f713ccc560367d5c14824c2242a7d8ce02
Romana-Aijaz <romanaaijaz99@gmail.com>
2024-07-03 13:47:29 +0300

    projects updated

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -54,7 +54,7 @@ export const BentoGridItem = ({
     spareImg?: string;
 }) => {
     const leftLists = ["ReactJS", "Express", "Typescript"];
-    const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+    const rightLists = ["Tailwind CSS", "NextJS", "React Native"];
 
     const [copied, setCopied] = useState(false);
 
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
     };
 
     const handleCopy = () => {
-        const text = "hsu@jsmastery.pro";
+        const text = "romanaaijaz99@gmail.com";
         navigator.clipboard.writeText(text);
         setCopied(true);
     };
--- data/index.ts
+++ data/index.ts
@@ -72,11 +72,11 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "/p1.svg",
+        title: "Scalable Online Retail Solution",
+        des: "Discover our cutting-edge eCommerce platform, seamlessly built with React.js, Express.js, and MongoDB.",
+        img: "/ecommerce2.png",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "https://github.com/adrianhajdin?tab=repositories",
+        link: "https://github.com/Romana-Aijaz/Ecommerce",
     },
     {
         id: 2,
